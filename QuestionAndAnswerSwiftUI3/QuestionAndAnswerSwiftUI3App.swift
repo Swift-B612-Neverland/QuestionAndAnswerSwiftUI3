dcdb714bc466a7be93ad8bea0ee8ed17de7fd146
Peter Pan <apppeterpan@gmail.com>
2025-09-09 14:37:36 +0800

    Initial Commit

---
+++ QuestionAndAnswerSwiftUI3/QuestionAndAnswerSwiftUI3App.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionAndAnswerSwiftUI3App.swift
+//  QuestionAndAnswerSwiftUI3
+//
+//  Created by SHIH-YING PAN on 2025/9/9.
+//
+
+import SwiftUI
+
+@main
+struct QuestionAndAnswerSwiftUI3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
